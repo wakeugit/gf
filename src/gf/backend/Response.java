a1bb953d0392053b709c19d8d9c0c70836f314e3
Lyonnel Dzotang <lyonnel.dzotang@wouri.co>
2016-10-20 11:21:10 +0100

    update response object

--- src/gf/backend/Response.java
+++ src/gf/backend/Response.java
@@ -1,12 +1,11 @@
 package gf.backend;
 
-import lombok.Data;
-
 import java.util.ArrayList;
 
-@Data
+
 public class Response<T> {
 
     T body;
     ArrayList<Exception> exceptions = new ArrayList<>();
+
 }
